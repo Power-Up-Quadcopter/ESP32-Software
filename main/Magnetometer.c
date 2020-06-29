54b208d1d631e3cd75d98aa9faec15b4bdf08e63
rbneiman <alecneiman@gmail.com>
2020-06-29 16:00:46 -0500

    Added magnetometer

--- main/Magnetometer.c
+++ main/Magnetometer.c
@@ -1,5 +1,27 @@
-//
-// Created by Alec on 6/29/2020.
-//
 
+#include <stdint.h>
+#include "I2C_General.h"
 #include "Magnetometer.h"
+
+#define mpuAddrW 208
+#define mpuAddrR 209
+
+void Mag_Write8(uint8_t data, uint8_t regAddr){
+    I2C_Write8(mpuAddrW, data, regAddr);
+}
+
+
+void Mag_Write16(uint16_t data, uint8_t regAddr){
+    I2C_Write16(mpuAddrW, data, regAddr);
+}
+
+uint8_t Mag_Read8(uint8_t regAddr){
+    uint8_t out = I2C_Read8(mpuAddrW, regAddr);
+    return out;
+}
+
+
+uint16_t Mag_Read16(uint8_t regAddr){
+    uint16_t out = I2C_Read16(mpuAddrW, regAddr);
+    return out;
+}
--- main/Magnetometer.h
+++ main/Magnetometer.h
@@ -1,8 +1,21 @@
-//
-// Created by Alec on 6/29/2020.
-//
 
 #ifndef ESP32_SOFTWARE_MAGNETOMETER_H
 #define ESP32_SOFTWARE_MAGNETOMETER_H
+
+//put useful defines for register addresses here
+
+//write an 8bit value to specific register
+void Mag_Write8(uint8_t data, uint8_t regAddr);
+
+//write a 16 bit value to two sequential registers
+//takes the high bit register b/c mpu auto increments register after r/w
+void Mag_Write16(uint16_t data, uint8_t regAddr);
+
+//read an 8bit value from specific register
+uint8_t Mag_Read8(uint8_t regAddr);
+
+//read a 16 bit value from two sequential registers
+//takes the high bit register b/c mpu auto increments register after r/w
+uint16_t Mag_Read16(uint8_t regAddr);
 
 #endif //ESP32_SOFTWARE_MAGNETOMETER_H
