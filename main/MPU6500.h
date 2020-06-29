0831673b4cda548d116af284f8a6a7766b90b4d3
rbneiman <alecneiman@gmail.com>
2020-06-29 15:12:22 -0500

    Added basic mpu6500 driver

---
+++ main/MPU6500.h
@@ -0,0 +1,19 @@
+
+#ifndef ESP32_SOFTWARE_MPU6500_H
+#define ESP32_SOFTWARE_MPU6500_H
+
+//write an 8bit value to specific register
+void MPU_Write8(uint8_t data, uint8_t regAddr);
+
+//write a 16 bit value to two sequential registers
+//takes the high bit register b/c mpu auto increments register after r/w
+void MPU_Write16(uint16_t data, uint8_t regAddr);
+
+//read an 8bit value from specific register
+uint8_t MPU_Read8(uint8_t regAddr);
+
+//read a 16 bit value from two sequential registers
+//takes the high bit register b/c mpu auto increments register after r/w
+uint16_t MPU_Read16(uint8_t regAddr);
+
+#endif //ESP32_SOFTWARE_MPU6500_H
