b0802cd0daf41822caaf743e7440aff9a98dc65f
Allen Jiang <alljiang@hotmail.com>
2020-07-04 22:18:53 -0500

    gps stuff in main

--- main/drone_main.cpp
+++ main/drone_main.cpp
@@ -1,7 +1,9 @@
 
 #include <stdio.h>
+#include <string>
 #include <nvs_flash.h>
 #include <tcpip_adapter.h>
+#include <sstream>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
@@ -16,6 +18,8 @@
 
 #define MAIN_LOOP_STACK_SIZE 2048
 
+using namespace std;
+
 extern "C" {
     void app_main();
 };
@@ -24,6 +28,8 @@ extern "C" {
 //Note: If not using OpenOCD, run using "mingw32-make.exe flash" in the terminal below
 
 void DroneLoop(void*);
+
+bool GPSready = false;
 
 uint8_t data[1024];
 char strbuffer[1024];
@@ -47,6 +53,7 @@ void task_gps(void *arg) {
     const int uart_num = UART_NUM_2;
     int length = 0;
 
+    GPSready = true;
     while(1) {
         int x = uart_get_buffered_data_len(uart_num, (size_t *) &length);
         ESP_ERROR_CHECK(x);
@@ -66,7 +73,23 @@ void task_gps(void *arg) {
     }
 }
 
+void sendGPS(string msg) {
+    uint8_t checksum = msg.data()[0];
+    for(int i = 1; i < msg.length(); i++) {
+        checksum ^= msg.data()[i];
+    }
+    stringstream sstream;
+    sstream << hex << checksum;
 
+    string toSend = "$";
+    toSend.append(msg);
+    toSend.append("*");
+    toSend.append(sstream.str());
+    printf("GPS SEND: %s\n", toSend.data());
+    toSend.append("\r\n");
+
+    uart_write_bytes(UART_NUM_2, toSend.data(), toSend.length());
+}
 
 //arduino setup function equivalent
 void app_main() {
@@ -81,25 +104,30 @@ void app_main() {
     ESP_ERROR_CHECK(ret);
 
 
-    Esc_Init();
     ESP_ERROR_CHECK(I2C_Init());
+//    Esc_Init();
 //    GPS_Init();
     Wifi_Init();
 
     //TEST CODE HERE
-    printf("TEST: Output 50 Percent in ESC0\n");
-    Esc_Set(0, 50);
+//    printf("TEST: Output 50 Percent in ESC0\n");
+//    Esc_Set(0, 50);
     //END TEST AREA
 
-    Wifi_startTCPServer();
+//    Wifi_startTCPServer();
 
     //create the DroneLoop task
-//    xTaskCreate(&DroneLoop, "DroneLoop", MAIN_LOOP_STACK_SIZE, NULL, 4, NULL);
+    xTaskCreate(&DroneLoop, "DroneLoop", MAIN_LOOP_STACK_SIZE, NULL, 4, NULL);
     xTaskCreate(&task_gps, "GPS", MAIN_LOOP_STACK_SIZE, NULL, 5, NULL);
 }
 
 //arduino loop function equivalent
 void DroneLoop(void* ptr){
+
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+    sendGPS("PSTMNMEAONOFF,0");
+    sendGPS("PSTMCFGANTSENS,2,1,0,0,0,1,8,150,500");
+
     while (1){
         //TODO put crap here
 
@@ -107,25 +135,28 @@ void DroneLoop(void* ptr){
         //printf("Looping\n");
         //END TEST AREA
 
+//        string msg = "PSTMCFGANTSENS,2,1,0,0,0,1,8,150,500";
+
+
+
+//  TCP Test message
 //        char message[] = "lololol\n";
 //        Wifi_sendTCP(message);
 
+//  I2C GPIO Expander LED Blink
+//        int chip_addr = 0x20;
+//        I2C_Write8(chip_addr, 0x00, 0b000);  //  set port A to outputs
+//        I2C_Write8(chip_addr, 0x01, 0b000);  //  set port B to outputs
+//        while(1) {
+//            printf("LEDR ON\n");
+//            I2C_Write8(chip_addr,0b101, 0x12);
+//            vTaskDelay(1000 / portTICK_RATE_MS);
+//            printf("LEDR OFF\n");
+//            I2C_Write8(chip_addr, 0b000, 0x12);
+//            vTaskDelay(1000 / portTICK_RATE_MS);
+//        }
 
-        int chip_addr = 0x20;
-        I2C_Write8(chip_addr, 0x00, 0b000);  //  set port A to outputs
-        I2C_Write8(chip_addr, 0x01, 0b000);  //  set port B to outputs
-
-//    /*
-        while(1) {
-            printf("LEDR ON\n");
-            I2C_Write8(chip_addr,0b101, 0x12);
-            vTaskDelay(1000 / portTICK_RATE_MS);
-            printf("LEDR OFF\n");
-            I2C_Write8(chip_addr, 0b000, 0x12);
-            vTaskDelay(1000 / portTICK_RATE_MS);
-        }
-
-        vTaskDelay(1000 / portTICK_PERIOD_MS); //delay 1000ms
+        vTaskDelay(5000 / portTICK_PERIOD_MS); //delay 1000ms
 
     }
 }
