03013494dc64e81c7b16677c9d58d1dc2a60148a
Allen Jiang <alljiang@hotmail.com>
2020-07-10 19:59:46 -0500

    maggot

--- main/Magnetometer.cpp
+++ main/Magnetometer.cpp
@@ -3,7 +3,7 @@
 #include "I2C_General.h"
 #include "Magnetometer.h"
 
-#define magAddrW 60
+#define magAddrW 0x1E
 #define MAG_LOOP_STACK_SIZE 2048
 
 MAGDATA_t magData;
@@ -15,7 +15,6 @@ void Mag_Write8(uint8_t data, uint8_t regAddr){
     I2C_Write8(magAddrW, data, regAddr);
 }
 
-
 void Mag_Write16(uint16_t data, uint8_t regAddr){
     I2C_Write16(magAddrW, data, regAddr);
 }
@@ -25,7 +24,6 @@ uint8_t Mag_Read8(uint8_t regAddr){
     return out;
 }
 
-
 uint16_t Mag_Read16(uint8_t regAddr){
     uint16_t out = I2C_Read16(magAddrW, regAddr);
     return out;
@@ -33,33 +31,37 @@ uint16_t Mag_Read16(uint8_t regAddr){
 
 void Mag_Init() {
     if(uint8_t i = Mag_Read8(MAG_WHOAMI)!=MAG_WHOAMI_VAL){
-        printf("Error for Mag device ID: Expected C7, Got %X\n", i);
+        printf("Error for Mag device ID: Expected %X, Got %X\n", MAG_WHOAMI_VAL, i);
         return;
     }
 
     //standby mode
-    Mag_Write8(0x0, MAG_M_CTRL_REG1);
+    Mag_Write8(0b00000000, MAG_CTRL_REG1);
+
     //auto-cal disabled
     //no one-shot mag reset
     //no one-shot mag measurement
     //8x oversampling
     //hybrid mode with accel and mag active
-    Mag_Write8(0x1F, MAG_CTRL_REG1);
+    Mag_Write8(0b00011111, MAG_M_CTRL_REG1);
+
     //hyb_autoinc_mode=1 mag registers follow accel registers
     //m_maxmin dis=0 default max/min latching
     //m_maxmin_dis_ths=0
     //m_maxmin_rst=0
     //m_rst_cnt=0 enable magnetic reset each cycle
-    Mag_Write8(0x20, MAG_CTRL_REG2);
+    Mag_Write8(0b00100000, MAG_M_CTRL_REG2);
+
     //hpf_out=0
     //fs=01 accel range of +/- 4g
     Mag_Write8(0x01, MAG_XYZ_DATA_CFG);
+
     //aslp_rate=00
     //dr=001 200hz data rate
     //lnoise=1 low noise mode
     //f_read=0 16 bit reads
     //active=1 take part out of standby
-    Mag_Write8(0x0D, MAG_M_CTRL_REG1);
+    Mag_Write8(0b00101101, MAG_CTRL_REG1);
 
     magData = MAGDATA_t{0,0,0,0};
     magReady = 1;
@@ -68,13 +70,19 @@ void Mag_Init() {
 
 void Mag_Loop(void* ptr){
     while (true){
-        while((Mag_Read8(MAG_M_DR_STATUS)&!0x8u)!=0){vTaskDelay(1 / portTICK_PERIOD_MS);} //delay until ready
-        magReady = 0;
+//        while((Mag_Read8(MAG_M_DR_STATUS) & (0b111) ) == 0){vTaskDelay(100 / portTICK_PERIOD_MS);} //delay until ready
+//        magReady = 1;
         magData.x = Mag_Read16(MAG_OUT_X);
         magData.y = Mag_Read16(MAG_OUT_Y);
         magData.z = Mag_Read16(MAG_OUT_Z);
         magData.temp = Mag_Read8(MAG_TEMP);
-        magReady = 1;
-        vTaskDelay(5 / portTICK_PERIOD_MS); //delay 5ms for 200hz
+
+        printf("X: %d\n", magData.x);
+        printf("Y: %d\n", magData.y);
+        printf("Z: %d\n", magData.z);
+        printf("Temp: %d\n", magData.temp);
+        printf("-----\n");
+
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
