9097970ce883ce73792589c6e58a64943e2f5d27
jason-math <jasonmath@utexas.edu>
2020-07-16 21:24:08 -0500

    Created Expander Driver

--- main/GPIO_Expand.cpp
+++ main/GPIO_Expand.cpp
@@ -3,24 +3,284 @@
 #include "I2C_General.h"
 #include "GPIO_Expand.h"
 
-#define expandAddrW 64
+#define expanderAddr 0x20
 
-void expand_Write8(uint8_t data, uint8_t regAddr){
-    I2C_Write8(expandAddrW, data, regAddr);
+void expander_init() {
+    // Set all pins default to output
+    I2C_Write8(expanderAddr, 0x00, 0x00);
+    I2C_Write8(expanderAddr, 0x01, 0x00);
+    //Configure pin behavior
+    I2C_Write8(expanderAddr, 0x0A, 0x00); //Check
+    I2C_Write8(expanderAddr, 0x0B, 0x00); //Check
+    // Set all PUR default to off
+    I2C_Write8(expanderAddr, 0x0C, 0x00);
+    I2C_Write8(expanderAddr, 0x0D, 0x00);
 }
 
-
-void expand_Write16(uint16_t data, uint8_t regAddr){
-    I2C_Write16(expandAddrW, data, regAddr);
+public GPIO_Expand() {
+    expander_init();
 }
 
-uint8_t expand_Read8(uint8_t regAddr){
-    uint8_t out = I2C_Read8(expandAddrW, regAddr);
-    return out;
+void set_dir_bit(uint8_t pin, uint8_t bit){
+
+    uint8_t port = -1;
+    // Determines if Port A or Port B
+    if(pin == this.A0 || pin == this.A1 || pin == this.A2 || pin = this.A3 || pin = this.A4 || pin == this.A5 || pin == this.A6 || pin = this.A7)
+        port = 0x00;
+    else if(pin == this.B0 || pin == this.B1 || pin == this.B2 || pin = this.B3 || pin = this.B4 || pin == this.B5 || pin == this.B6 || pin = this.B7)
+        port = 0x01;
+
+    if(port == -1)
+        return;
+    
+    uint8_t data = I2C_Read8(expanderAddr, port);
+    // Sets the correct bit
+    if(pin == this.A0 || pin == this.B0);
+    else if(pin == this.A1 || pin == this.B1)
+        bit = bit << 1;
+    else if(pin == this.A2 || pin == this.B2)
+        bit = bit << 2;
+    else if(pin == this.A3 || pin == this.B3)
+        bit = bit << 3;
+    else if(pin == this.A4 || pin == this.B4)
+        bit = bit << 4;
+    else if(pin == this.A5 || pin == this.B5)
+        bit = bit << 5;
+    else if(pin == this.A6 || pin == this.B6)
+        bit = bit << 6;
+    else if(pin == this.A7 || pin == this.B7)
+        bit = bit << 7;
+    data |= bit;
+    data &= bit;
+    // Writes the new value to the dir register
+    I2C_Write8(expandAddrW, data, port);
 }
 
+void set_dir_reg(uint8_t byte, uint8_t port){
+    // Writes to the entire dir register
+    if(port == this.A)
+        I2C_Write8(expanderAddr, byte, 0x00);
+    else if(port == this.B)
+        I2C_Write8(expanderAddr, byte, 0x01);
+}
 
-uint16_t expand_Read16(uint8_t regAddr){
-    uint16_t out = I2C_Read16(expandAddrW, regAddr);
-    return out;
+uint8_t get_dir_bit(uint8_t pin){
+
+    uint8_t port = -1;
+    // Determines if Port A or Port B
+    if(pin == this.A0 || pin == this.A1 || pin == this.A2 || pin = this.A3 || pin = this.A4 || pin == this.A5 || pin == this.A6 || pin = this.A7)
+        port = 0x00;
+    else if(pin == this.B0 || pin == this.B1 || pin == this.B2 || pin = this.B3 || pin = this.B4 || pin == this.B5 || pin == this.B6 || pin = this.B7)
+        port = 0x01;
+
+    if(port == -1)
+        return -1;
+    // reads bit
+    if(pin == this.A0)
+        return (I2C_Read8(expanderAddr, port) & 0x01);
+    else if(pin == this.A1)
+        return (I2C_Read8(expanderAddr, port) & 0x02) >> 1;
+    else if(pin == this.A2)
+        return (I2C_Read8(expanderAddr, port) & 0x04) >> 2;
+    else if(pin == this.A3)
+        return (I2C_Read8(expanderAddr, port) & 0x08) >> 3;
+    else if(pin == this.A4)
+        return (I2C_Read8(expanderAddr, port) & 0x10) >> 4;
+    else if(pin == this.A5)
+        return (I2C_Read8(expanderAddr, port) & 0x20) >> 5;
+    else if(pin == this.A6)
+        return (I2C_Read8(expanderAddr, port) & 0x40) >> 6;
+    else if(pin == this.A7)
+        return (I2C_Read8(expanderAddr, port) & 0x80) >> 7;
+    else
+        return -1;
+}
+
+uint8_t get_dir_reg(uint8_t port){
+    // Reads the entire dir register
+    if(port == this.A)
+        return I2C_Read8(expanderAddr, 0x00);
+    else if(port == this.B)
+        return I2C_Read8(expanderAddr, 0x01);
+}
+
+void set_pur_bit(uint8_t pin, uint8_t bit){
+
+    uint8_t port = -1;
+    // Determines if Port A or Port B
+    if(pin == this.A0 || pin == this.A1 || pin == this.A2 || pin = this.A3 || pin = this.A4 || pin == this.A5 || pin == this.A6 || pin = this.A7)
+        port = 0x0C;
+    else if(pin == this.B0 || pin == this.B1 || pin == this.B2 || pin = this.B3 || pin = this.B4 || pin == this.B5 || pin == this.B6 || pin = this.B7)
+        port = 0x0D;
+
+    if(port == -1)
+        return;
+
+    uint8_t data = I2C_Read8(expanderAddr, port);
+    // Sets the correct bit
+    if(pin == this.A0 || pin == this.B0);
+    else if(pin == this.A1 || pin == this.B1)
+        bit = bit << 1;
+    else if(pin == this.A2 || pin == this.B2)
+        bit = bit << 2;
+    else if(pin == this.A3 || pin == this.B3)
+        bit = bit << 3;
+    else if(pin == this.A4 || pin == this.B4)
+        bit = bit << 4;
+    else if(pin == this.A5 || pin == this.B5)
+        bit = bit << 5;
+    else if(pin == this.A6 || pin == this.B6)
+        bit = bit << 6;
+    else if(pin == this.A7 || pin == this.B7)
+        bit = bit << 7;
+    data |= bit;
+    data &= bit;
+    //Writes the new value to the pur register
+    I2C_Write8(expandAddrW, data, port);
+}
+
+void set_pur_reg(uint8_t byte, uint8_t port){
+    // Reads entire pur register
+    if(port == this.A)
+        I2C_Write8(expanderAddr, byte, 0x0C);
+    else if(port == this.B)
+        I2C_Write8(expanderAddr, byte, 0x0D);
+}
+
+uint8_t read_bit(uint8_t pin) {
+    uint8_t port = -1;
+    // Determines if Port A or Port B
+    if(pin == this.A0 || pin == this.A1 || pin == this.A2 || pin = this.A3 || pin = this.A4 || pin == this.A5 || pin == this.A6 || pin = this.A7)
+        port = 0x13;
+    else if(pin == this.B0 || pin == this.B1 || pin == this.B2 || pin = this.B3 || pin = this.B4 || pin == this.B5 || pin == this.B6 || pin = this.B7)
+        port = 0x14;
+
+    if(port == -1)
+        return -1;
+    // reads bit
+    if(pin == this.A0 || pin == this.B0)
+        return (I2C_Read8(expanderAddr, port) & 0x01);
+    else if(pin == this.A1 || pin == this.B1)
+        return (I2C_Read8(expanderAddr, port) & 0x02) >> 1;
+    else if(pin == this.A2 || pin == this.B2)
+        return (I2C_Read8(expanderAddr, port) & 0x04) >> 2;
+    else if(pin == this.A3 || pin == this.B3)
+        return (I2C_Read8(expanderAddr, port) & 0x08) >> 3;
+    else if(pin == this.A4 || pin == this.B4)
+        return (I2C_Read8(expanderAddr, port) & 0x10) >> 4;
+    else if(pin == this.A5 || pin == this.B5)
+        return (I2C_Read8(expanderAddr, port) & 0x20) >> 5;
+    else if(pin == this.A6 || pin == this.B6)
+        return (I2C_Read8(expanderAddr, port) & 0x40) >> 6;
+    else if(pin == this.A7 || pin == this.B7)
+        return (I2C_Read8(expanderAddr, port) & 0x80) >> 7;
+    else
+        return -1;
+}
+
+uint8_t read_port(uint8_t port){
+    // Reads entire port
+    if(port == this.A)
+        return I2C_Read8(expanderAddr, 0x13);
+    else if(port == this.B)
+        return I2C_Read8(expanderAddr, 0x14);
+}
+
+void set_bit(uint8_t pin, uint8_t bit){
+
+    uint8_t port = -1;
+    // Determines if Port A or Port B
+    if(pin == this.A0 || pin == this.A1 || pin == this.A2 || pin = this.A3 || pin = this.A4 || pin == this.A5 || pin == this.A6 || pin = this.A7)
+        port = 0x13;
+    else if(pin == this.B0 || pin == this.B1 || pin == this.B2 || pin = this.B3 || pin = this.B4 || pin == this.B5 || pin == this.B6 || pin = this.B7)
+        port = 0x14;
+
+    if(port == -1)
+        return;
+
+    uint8_t data = I2C_Read8(expanderAddr, port);
+    // Sets the correct bit and checks the direction
+    if(pin == this.A0) {
+        if (this.ADIR & 0x01)
+            return;
+    }
+    else if(pin == this.A1){
+        if(this.ADIR & 0x02)
+            return;
+        bit = bit << 1;
+    }
+    else if(pin == this.A2){
+        if(this.ADIR & 0x04)
+            return;
+        bit = bit << 2;
+    }
+    else if(pin == this.A3){
+        if(this.ADIR & 0x08)
+            return;
+        bit = bit << 3;
+    }
+    else if(pin == this.A4){
+        if(this.ADIR & 0x10)
+            return;
+        bit = bit << 4;
+    }
+    else if(pin == this.A5){
+        if(this.ADIR & 0x20)
+            return;
+        bit = bit << 5;
+    }
+    else if(pin == this.A6){
+        if(this.ADIR & 0x40)
+            return;
+        bit = bit << 6;
+    }
+    else if(pin == this.A7){
+        if(this.ADIR & 0x80)
+            return;
+        bit = bit << 7;
+    }
+    if(pin == this.B0) {
+        if (this.BDIR & 0x01)
+            return;
+    }
+    else if(pin == this.B1){
+        if(this.BDIR & 0x02)
+            return;
+        bit = bit << 1;
+    }
+    else if(pin == this.B2){
+        if(this.BDIR & 0x04)
+            return;
+        bit = bit << 2;
+    }
+    else if(pin == this.B3){
+        if(this.BDIR & 0x08)
+            return;
+        bit = bit << 3;
+    }
+    else if(pin == this.B4){
+        if(this.BDIR & 0x10)
+            return;
+        bit = bit << 4;
+    }
+    else if(pin == this.B5){
+        if(this.BDIR & 0x20)
+            return;
+        bit = bit << 5;
+    }
+    else if(pin == this.B6){
+        if(this.BDIR & 0x40)
+            return;
+        bit = bit << 6;
+    }
+    else if(pin == this.B7){
+        if(this.BDIR & 0x80)
+            return;
+        bit = bit << 7;
+    }
+
+    data |= bit;
+    data &= bit;
+    // Writes the new value to the GPIO port register
+    I2C_Write8(expandAddrW, data, port);
 }
--- main/GPIO_Expand.h
+++ main/GPIO_Expand.h
@@ -10,18 +10,47 @@
 #define GPIOA 0x12
 #define GPIOB 0x13
 
-//write an 8bit value to specific register
-void expand_Write8(uint8_t data, uint8_t regAddr);
 
-//write a 16 bit value to two sequential registers
-//takes the high bit register b/c mpu auto increments register after r/w
-void expand_Write16(uint16_t data, uint8_t regAddr);
+class GPIO_Expand {
 
-//read an 8bit value from specific register
-uint8_t expand_Read8(uint8_t regAddr);
+public:
+    uint8_t A0 = 0, A1 = 1, A2 = 2, A3 = 3, A4 = 4, A5 = 5, A6 = 6, A7 = 7;
+    uint8_t B0 = 8, B1 = 9, B2 = 10, B3 = 11, B4 = 12, B5 = 13, B6 = 14, B7 = 15;
+    uint8_t A = 16, B = 17;
+    uint8_t ADIR = 0, BDIR = 0;
 
-//read a 16 bit value from two sequential registers
-//takes the high bit register b/c mpu auto increments register after r/w
-uint16_t expand_Read16(uint8_t regAddr);
+    // Initializes registers to their initial state
+    void expander_init()
+
+    // Sets the I/O direction of a specific pin to the given bit value
+    void set_dir_bit(uint8_t pin, uint8_t bit)
+
+    // Sets the entire Direction register of the specified port (A or B)
+    void set_dir_reg(uint8_t byte, uint8_t port)
+
+    // Gets a specific direction bit
+    uint8_t get_dir_bit(uint8_t pin)
+
+    // Gets the entire bit pattern of the Direction register
+    uint8_t get_dir_reg(uint8_t port)
+
+    // Sets the Pull Up Resistor bit of a specific pin to the given bit value
+    void set_pur_bit(uint8_t pin, uint8_t bit)
+
+    // Sets the entire bit pattern of the Pull Up Resistor Register
+    void set_pur_reg(uint8_t byte, uint8_t port)
+
+    // Reads a specific GPIO bit
+    uint8_t read_bit(uint8_t pin)
+
+    // Reads the entire bit pattern of the GPIO register data
+    uint8_t read_port(uint8_t port)
+
+    // Sets the specified output bit of the GPIO register data
+    void set_bit(uint8_t pin, uint8_t bit)
+
+};
+
+
 
 #endif //ESP32_SOFTWARE_GPIO_EXPAND_H
