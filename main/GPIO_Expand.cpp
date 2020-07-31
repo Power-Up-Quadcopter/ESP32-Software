036d14fc37713c245adf3580e61c30388239cefe
rbneiman <alecneiman@gmail.com>
2020-07-31 15:39:24 -0500

    GPIO driver added

--- main/GPIO_Expand.cpp
+++ main/GPIO_Expand.cpp
@@ -7,16 +7,25 @@
 
 namespace Expand {
 
+    uint8_t portA;
+    uint8_t portB;
+
     void init() {
         // Set all pins default to output
-        I2C_Write8(expandAddrW, 0x00, 0x00);
-        I2C_Write8(expandAddrW, 0x00, 0x01);
+        I2C_Write8(expandAddrW, 0x00, IODIRA);
+        I2C_Write8(expandAddrW, 0x00, IODIRB);
         //Configure pin behavior
         I2C_Write8(expandAddrW, 0x00, 0x0A); //Check
         I2C_Write8(expandAddrW, 0x00, 0x0B); //Check
         // Set all PUR default to off
-        I2C_Write8(expandAddrW, 0x00, 0x0C);
-        I2C_Write8(expandAddrW, 0x00, 0x0D);
+        I2C_Write8(expandAddrW, 0x00, GPPUA);
+        I2C_Write8(expandAddrW, 0x00, GPPUB);
+        //all pins low
+        I2C_Write8(expandAddrW, 0x00, GPIOA);
+        I2C_Write8(expandAddrW, 0x00, GPIOB);
+
+        portA = 0;
+        portB = 0;
     }
 
     void write8(uint8_t data, uint8_t regAddr) {
@@ -37,6 +46,38 @@ namespace Expand {
     uint16_t read16(uint8_t regAddr) {
         uint16_t out = I2C_Read16(expandAddrW, regAddr);
         return out;
+    }
+
+    void setPinDir(pin_t pin, bool dir) {
+        uint8_t addr = (pin > 7) ? IODIRA : IODIRB;
+        uint8_t offset = (pin > 7) ? pin : pin - 8;
+        uint8_t *port = (pin > 7) ? &portA : &portB;
+
+        *port = (*port & ~(0x1u << offset)) | (dir << offset);
+        write8(*port, addr);
+    }
+
+    void setPinData(pin_t pin, bool on){
+        uint8_t addr = (pin>7) ? GPIOA : GPIOB;
+        uint8_t offset = (pin>7) ? pin : pin-8;
+        uint8_t *port = (pin>7) ? &portA : &portB;
+
+        *port = (*port & ~(0x1u << offset)) | (on << offset);
+        write8(*port, addr);
+    }
+
+    void setAllDir(uint8_t newPortA, uint8_t newPortB){
+        portA = newPortA;
+        portB = newPortB;
+        write8(portA, IODIRA);
+        write8(portB, IODIRB);
+    }
+
+    void setAllData(uint8_t newPortA, uint8_t newPortB){
+        portA = newPortA;
+        portB = newPortB;
+        write8(portA, GPIOA);
+        write8(portB, GPIOB);
     }
 
 }
--- main/GPIO_Expand.h
+++ main/GPIO_Expand.h
@@ -3,30 +3,66 @@
 #define ESP32_SOFTWARE_GPIO_EXPAND_H
 
 namespace Expand{
-//put useful defines for register addresses here
-#define IODIRA 0x00
-#define IODIRB 0x01
-#define GPPUA 0x0C
-#define GPPUB 0x0D
-#define GPIOA 0x12
-#define GPIOB 0x13
 
-//Initialize all to output, PUR off
-void init();
+    //put useful defines for register addresses here
+    #define IODIRA 0x00
+    #define IODIRB 0x01
+    #define GPPUA 0x0C
+    #define GPPUB 0x0D
+    #define GPIOA 0x12
+    #define GPIOB 0x13
 
-//write an 8bit value to specific register
-void write8(uint8_t data, uint8_t regAddr);
+    extern uint8_t portA;
+    extern uint8_t portB;
 
-//write a 16 bit value to two sequential registers
-//takes the high bit register b/c mpu auto increments register after r/w
-void write16(uint16_t data, uint8_t regAddr);
+    typedef enum{
+        PIN0_A = 0,
+        PIN1_A = 1,
+        PIN2_A = 2,
+        PIN3_A = 3,
+        PIN4_A = 4,
+        PIN5_A = 5,
+        PIN6_A = 6,
+        PIN7_A = 7,
+        PIN0_B = 8,
+        PIN1_B = 9,
+        PIN2_B = 10,
+        PIN3_B = 11,
+        PIN4_B = 12,
+        PIN5_B = 13,
+        PIN6_B = 14,
+        PIN7_B = 15
+    }pin_t;
 
-//read an 8bit value from specific register
-uint8_t read8(uint8_t regAddr);
 
-//read a 16 bit value from two sequential registers
-//takes the high bit register b/c mpu auto increments register after r/w
-uint16_t read16(uint8_t regAddr);
+    //Initialize all to output, PUR off, level low
+    void init();
+
+    //write an 8bit value to specific register
+    void write8(uint8_t data, uint8_t regAddr);
+
+    //write a 16 bit value to two sequential registers
+    //takes the high bit register b/c mpu auto increments register after r/w
+    void write16(uint16_t data, uint8_t regAddr);
+
+    //read an 8bit value from specific register
+    uint8_t read8(uint8_t regAddr);
+
+    //read a 16 bit value from two sequential registers
+    //takes the high bit register b/c mpu auto increments register after r/w
+    uint16_t read16(uint8_t regAddr);
+
+    //set a single pin's direction
+    void setPinDir(pin_t pin, bool dir);
+
+    //set a single pin to high or low
+    void setPinData(pin_t pin, bool on);
+
+    //set all pins' directions
+    void setAllDir(uint8_t newPortA, uint8_t newPortB);
+
+    //set all pins' level
+    void setAllData(uint8_t newPortA, uint8_t newPortB);
 
 }
 #endif //ESP32_SOFTWARE_GPIO_EXPAND_H
